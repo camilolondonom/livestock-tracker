767680b2e86d2823567d49376378e20fa4a6e47f
camilolondonom <clm.imdg@gmail.com>
2026-02-05 13:30:10 -0500

    chore: punto de control antes de organizar ramas

---
+++ backend/src/main/java/com/livestocktracker/controller/UsuarioController.java
@@ -0,0 +1,36 @@
+package com.livestocktracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.livestocktracker.model.Usuario;
+import com.livestocktracker.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "*") 
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @PostMapping
+public Usuario guardarUsuario(@RequestBody Usuario usuario) {
+    if (usuario == null) {
+        throw new IllegalArgumentException("El usuario no puede ser nulo");
+    }
+    return usuarioRepository.save(usuario);
+}
+}
